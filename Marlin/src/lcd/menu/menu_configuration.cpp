d558bf7dad08fa77534f3c357e026663989f213d
Victor <victorrar.ru@gmail.com>
2020-11-16 17:18:07 +0200

    disabled fast pwm, changed retract precision

--- Marlin/src/lcd/menu/menu_configuration.cpp
+++ Marlin/src/lcd/menu/menu_configuration.cpp
@@ -293,7 +293,7 @@ void menu_advanced_settings();
     #endif
     EDIT_ITEM(float3, MSG_CONTROL_RETRACTF, &fwretract.settings.retract_feedrate_mm_s, 1, 999);
     EDIT_ITEM(float52sign, MSG_CONTROL_RETRACT_ZHOP, &fwretract.settings.retract_zraise, 0, 999);
-    EDIT_ITEM(float52sign, MSG_CONTROL_RETRACT_RECOVER, &fwretract.settings.retract_recover_extra, -100, 100);
+    EDIT_ITEM(float43, MSG_CONTROL_RETRACT_RECOVER, &fwretract.settings.retract_recover_extra, -1, 1);
     #if HAS_MULTI_EXTRUDER
       EDIT_ITEM(float52sign, MSG_CONTROL_RETRACT_RECOVER_SWAP, &fwretract.settings.swap_retract_recover_extra, -100, 100);
     #endif
